c967f57183b470340cb74b489e20bf6b534752fc
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-25 14:35:09 +0100

    pundit

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,2 +1,47 @@
 class FlatsController < ApplicationController
+def new
+    @flat = Flat.new
+    authorize @flat
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.user = current_user
+    authorize @flat
+
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @flat.update(flat_params)
+    authorize @flat
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @flat.destroy
+    authorize @flat
+    redirect_to flats_path
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit( :address, :typology, :area, :rent, :rental_charges, :curent_state, :appartement_condition, :message )
+  end
 end
