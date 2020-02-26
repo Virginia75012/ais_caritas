9d4abdd38ed2dc945ad9fda8099be44e76bf05aa
Ashley Hozé <ashleyhoze@hotmail.fr>
2020-02-26 16:04:00 +0100

    autocomplite address

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_103629) do
+ActiveRecord::Schema.define(version: 2020_02_26_142557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 2020_02_26_103629) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "origin_contact"
     t.text "title"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
