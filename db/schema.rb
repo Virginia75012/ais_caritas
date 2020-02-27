2c220d15cbfa7707a8bb495121aa2a92ce41b075
Virginia Atenza <virginia.atenza@gmail.com>
2020-02-27 10:27:11 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_26_142557) do
-
+ActiveRecord::Schema.define(version: 2020_02_26_144949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,7 +53,6 @@ ActiveRecord::Schema.define(version: 2020_02_26_142557) do
     t.string "title"
     t.float "latitude"
     t.float "longitude"
-
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
