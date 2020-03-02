d1ed49b3d3c009ab625ef9a1ee9a7c44c58d0990
Virginia Atenza <virginia.atenza@gmail.com>
2020-03-02 13:21:54 +0100

    init simulateur with data-set

--- app/javascript/components/init_simulateur.js
+++ app/javascript/components/init_simulateur.js
@@ -1,4 +1,5 @@
 const initSimulateur = () => {
+
   const removeActive = () => {
     document.querySelectorAll('.zone').forEach((btn) => {
       btn.classList.remove('btn-active')
@@ -14,19 +15,64 @@ const initSimulateur = () => {
     btn.addEventListener('click', toggleActive)
   })
 
-  const supression = () => {
+  const supressionActive = () => {
     document.querySelectorAll('.percentage').forEach((btn) => {
       btn.classList.remove('btn-active')
     })
   }
 
-  const change = (event) => {
-    supression();
+  const changeActive = (event) => {
+    supressionActive();
     event.currentTarget.classList.add('btn-active')
   }
 
   document.querySelectorAll('.percentage').forEach((btn) => {
-    btn.addEventListener('click', change)
+    btn.addEventListener('click', changeActive)
   })
+
+  const setZone = (event) => {
+    document.querySelector('#state').dataset.zone = event.currentTarget.innerText
+  }
+
+  const setPerc = (event) => {
+    document.querySelector('#state').dataset.percentage = event.currentTarget.innerText.split('%')[0]
+  }
+
+  const simulation = () => {
+    const state = document.querySelector('#state')
+
+    document.querySelector('#myRange').oninput = function() {
+      state.dataset.m2 = this.value;
+    }
+
+    document.querySelectorAll('.zone').forEach((zone) => {
+      zone.addEventListener('click', setZone)
+    })
+
+    document.querySelectorAll('.percentage').forEach((perc) => {
+      perc.addEventListener('click', setPerc)
+    })
+
+
+    console.log(state.dataset.zone)
+    console.log(state.dataset.m2)
+    console.log(state.dataset.percentage)
+
+    if (state.dataset.zone === 'ZONE A' && state.dataset.percentage === '30') {
+
+    } else if (zone.innerText === 'ZONE A' && percentage.innerText === '70') {
+
+    } else if (zone.innerText === 'ZONE A' && percentage.innerText === '85') {
+
+    }
+  }
+
+  const items = document.querySelectorAll('.simulation')
+
+  items.forEach((item) => {
+    item.addEventListener('click', simulation)
+  })
+
 }
- export { initSimulateur };
+
+export { initSimulateur };
