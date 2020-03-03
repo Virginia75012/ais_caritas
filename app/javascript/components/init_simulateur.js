53facf8a2928923a6eb0fe96a2ead71132c68e23
pamela prieur <pamela.prieur@gmail.com>
2020-03-03 13:03:55 +0100

    counter done

--- app/javascript/components/init_simulateur.js
+++ app/javascript/components/init_simulateur.js
@@ -1,4 +1,6 @@
 const initSimulateur = () => {
+  let minRent;
+  let maxRent;
 
   const removeActive = () => {
     document.querySelectorAll('.zone').forEach((btn) => {
@@ -54,34 +56,53 @@ const initSimulateur = () => {
       perc.addEventListener('click', setPerc)
     })
 
-    const minRent = document.getElementById('min');
-    const maxRent = document.getElementById('max');
+    const minHtml = document.getElementById('min');
+    const maxHtml = document.getElementById('max');
     document.getElementById("demo").innerHTML = state.dataset.m2 + "m²";
 
     if (state.dataset.zone === 'ZONE A' && state.dataset.percentage === '30') {
-      minRent.innerHTML = 17 * state.dataset.m2;
-      maxRent.innerHTML = 19 * state.dataset.m2;
+      minRent = 17 * state.dataset.m2;
+      maxRent = 19 * state.dataset.m2;
 
     } else if (state.dataset.zone === 'ZONE A' && state.dataset.percentage === '70') {
-      minRent.innerHTML = 13 * state.dataset.m2;
-      maxRent.innerHTML = 15 * state.dataset.m2;
+      minRent = 13 * state.dataset.m2;
+      maxRent = 15 * state.dataset.m2;
 
     } else if (state.dataset.zone === 'ZONE A' && state.dataset.percentage === '85') {
-      minRent.innerHTML = 10 * state.dataset.m2;
-      maxRent.innerHTML = 12 * state.dataset.m2;
+      minRent = 10 * state.dataset.m2;
+      maxRent = 12 * state.dataset.m2;
 
     } else if (state.dataset.zone === 'ZONE Abis' && state.dataset.percentage === '30') {
-      minRent.innerHTML = 18 * state.dataset.m2;
-      maxRent.innerHTML = 20 * state.dataset.m2;
+      minRent = 18 * state.dataset.m2;
+      maxRent = 20 * state.dataset.m2;
 
     } else if (state.dataset.zone === 'ZONE Abis' && state.dataset.percentage === '70'){
-      minRent.innerHTML = 17 * state.dataset.m2;
-      maxRent.innerHTML = 19 * state.dataset.m2;
+      minRent = 17 * state.dataset.m2;
+      maxRent = 19 * state.dataset.m2;
 
     } else if (state.dataset.zone === 'ZONE Abis' && state.dataset.percentage === '85'){
-      minRent.innerHTML = 15 * state.dataset.m2;
-      maxRent.innerHTML = 17 * state.dataset.m2;
+      minRent = 15 * state.dataset.m2;
+      maxRent = 17 * state.dataset.m2;
     }
+
+    counter(minHtml, minRent)
+    counter(maxHtml, maxRent)
+  }
+
+  const counter = (elementHtml, value) => {
+    const n = parseInt(value)
+    let cpt = -1; // Initialisation du compteur
+    const duree = 4; // Durée en seconde pendant laquel le compteur ira de 0 à 15
+    const delta = Math.ceil((duree * 1000) / n); // On calcule l'intervalle de temps entre chaque rafraîchissement du compteur (durée mise en milliseconde)
+
+    function countdown() {
+      elementHtml.innerHTML = ++cpt;
+      if( cpt < n ) { // Si on est pas arrivé à la valeur finale, on relance notre compteur une nouvelle fois
+         setTimeout(countdown, delta);
+      }
+    }
+
+    setTimeout(countdown, delta);
   }
 
   const items = document.querySelectorAll('.simulation')
